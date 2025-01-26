253a09ba998d330077dae48e696d6e6c8bf9cab8
Mario Lopez <mario@techmunchies.net>
2025-01-26 12:58:01 -0500

    fix schedule, tweak to install packages only on desktop release, comment out cosmic for now

--- build.sh
+++ build.sh
@@ -8,16 +8,33 @@ echo "::group:: ===Remove CLI Wrap==="
 /ctx/remove-cliwrap.sh
 echo "::endgroup::"
 
-echo "::group:: ===Desktop Packages==="
-/ctx/desktop-packages.sh
-echo "::endgroup::"
+case "${IMAGE}" in
+"bazzite"*)
+    echo "::group:: ===Desktop Packages==="
+    /ctx/desktop-packages.sh
+    echo "::endgroup::"
 
-echo "::group:: ===Remove Packages==="
-/ctx/remove-packages.sh
-echo "::endgroup::"
+    echo "::group:: ===Remove Packages==="
+    /ctx/remove-packages.sh
+    echo "::endgroup::"
 
-echo "::group:: ===Additional tweaks==="
-echo "::endgroup::"
+    echo "::group:: ===Additional tweaks==="
+    echo "::endgroup::"
+    ;;
+"cosmic"*)
+    echo "::group:: ===Desktop Packages==="
+    /ctx/desktop-packages.sh
+    echo "::endgroup::"
+
+    echo "::group:: ===Remove Packages==="
+    /ctx/remove-packages.sh
+    echo "::endgroup::"
+
+    echo "::group:: ===Additional tweaks==="
+    echo "::endgroup::"
+    ;;
+"ucore"*) ;;
+esac
 
 echo "::group:: ===Branding Changes==="
 /ctx/branding.sh
